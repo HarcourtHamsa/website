dd4f7e8f4ac2a532c7843467eaa886d58656b126
Harcourt Hamsa <hamsaharcourt@gmail.com>
2019-03-02 17:23:23 +0100

    add functionalities

--- index.py
+++ index.py
@@ -1,5 +1,17 @@
 def get_Api():
     pass
 
-def read_file_contents(file):
-    pass
+def read_file_contents():
+    """ This function reads data from a file and prints it out """
+    try:
+        file  =  input("Provide a file name : ")
+        open_file  =  open(file, "r")
+        file_data  = open_file.readlines()
+        return file_data
+
+    except FileNotFoundError:
+        print ("File Not Found DickHead")
+
+
+    
+read_file_contents()
