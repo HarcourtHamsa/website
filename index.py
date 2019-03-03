9d44789ce8cbd738a535a1729a2a2f1054af58ac
Harcourt Hamsa <hamsaharcourt@gmail.com>
2019-03-03 18:06:07 +0100

    refactor code

--- index.py
+++ index.py
@@ -1,17 +1,15 @@
-def get_Api():
-    pass
-
 def read_file_contents():
     """ This function reads data from a file and prints it out """
     try:
-        file  =  input("Provide a file name : ")
-        open_file  =  open(file, "r")
-        file_data  = open_file.readlines()
-        return file_data
+        file = input("Enter the filename: ")
+        with open(file,"r") as read_file:
+            file_contents = read_file.readlines()
+            return file_contents
 
     except FileNotFoundError:
-        print ("File Not Found DickHead")
+        print ("File Is Not Found DickHead")
 
 
     
-read_file_contents()
+result =  read_file_contents()
+print(result)
